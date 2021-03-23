07378f93842289d321d1786ddfabcd2f42e830e7
LukasLawler <headfirstluke377@gmail.com>
2021-03-22 20:49:35 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ValidationLawler.Models;
+
+namespace ValidationLawler.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Display(RegistrationModel registration)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name = registration.Name;
+                ViewBag.Phone = registration.PhoneNo;
+                ViewBag.Address = registration.Address;
+                ViewBag.Email = registration.EmailAddress;
+                ViewBag.Age = registration.Age;
+                return View(registration);
+            }
+            else
+            {
+                return View("Index", registration);
+            }
+        }
+    }
+}
---
+++ Models/RegistrationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ValidationLawler.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone]
+        public string PhoneNo { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+        [Required(ErrorMessage = "Age is required")]
+        [Range(18, 200, ErrorMessage = "Must be over 18 to register. Ages over 200 are not allowed.")]
+        public int Age { get; set; }
+    }
+}
